92b80546e1d80c4410e304194af292473a2bdb47
Trung Nguyen Thanh <thanhtrung692k@gmail.com>
2024-10-13 17:06:37 +0700

    code

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 'apps.core',
+    'apps.accounts',
+    'apps.match',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +125,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-print(f"DEBUG: {DEBUG}")
-print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
